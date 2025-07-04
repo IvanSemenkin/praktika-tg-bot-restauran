b3aafba4f9379eb2baffc0529448bf0219375998
IvanSemenkin <ismenkin@gmail.com>
2025-07-04 10:26:30 +0300

    tets

---
+++ src/storage/utils/log_user_action.py
@@ -0,0 +1,5 @@
+def log_user_action(message, prefix=""):
+    username = message.from_user.username or f"id_{message.from_user.id}"
+    full_name = message.from_user.first_name
+    last_name = message.from_user.last_name
+    return f"{prefix} Пользователь: {full_name} {last_name} (@{username}), ID: {message.from_user.id}"
